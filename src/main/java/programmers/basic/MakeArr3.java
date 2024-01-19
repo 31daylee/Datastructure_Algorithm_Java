c354f5706fe6edc568896e1bf33e56d18d45fa94
gussla2206 <gussla2206@gmail.com>
2024-01-19 23:54:33 +0900

    DAY12 배열만들기3

---
+++ src/main/java/programmers/basic/MakeArr3.java
@@ -0,0 +1,18 @@
+package programmers.basic;
+
+import java.util.ArrayList;
+
+/**
+ * 날짜: 2024/01/19
+ * 내용: 코딩 기초 트레이닝_배열 만들기 3
+ */
+public class MakeArr3 {
+    public int[] solution(int[] arr, int[][] intervals) {
+        ArrayList<Integer> result = new ArrayList<>();
+
+        for(int i=0; i<intervals.length; i++){
+            for(int j=intervals[i][0]; j<=intervals[i][1]; j++) result.add(arr[j]);
+        }
+        return result.stream().mapToInt(i->i).toArray();
+    }
+}
